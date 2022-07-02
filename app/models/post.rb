c043524781c33a56c8fc057fa4822ea8e53d8206
Dennis Ojeda <royojeda1@gmail.com>
2022-07-02 18:20:00 +0800

    Add the association between User and Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true
   validates :link, presence: true
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :username, length: { in: 4..12 }, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
   validates :password, length: { minimum: 8 }, confirmation: true, presence: true
+
+  has_many :posts
 end
