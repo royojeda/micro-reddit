9744dc3c58370d62c21f6252284e2b8333b07412
Dennis Ojeda <royojeda1@gmail.com>
2022-07-02 17:52:08 +0800

    Add a Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_02_090804) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_02_094355) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "link"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -19,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_02_090804) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
