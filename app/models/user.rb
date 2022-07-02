8e88630f44b594b7cef9db616f3e1a1af89cc98d
Dennis Ojeda <royojeda1@gmail.com>
2022-07-02 17:43:06 +0800

    Add validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, length: { in: 4..12 }, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, length: { minimum: 8 }, confirmation: true, presence: true
 end
